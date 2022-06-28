297db7c8b1c4b7d585cdbf28a5421be0de21f382
991 <72177472+Ruan991@users.noreply.github.com>
2022-06-27 22:31:45 -0300

    Add files via upload

--- faq/script.js
+++ faq/script.js
@@ -6,13 +6,13 @@ for(let i=0; i<toggles.length; i++){
     toggles[i].addEventListener('click', ()=>{
         if( parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight){
             contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
-            toggles[i].style.color = "#0084e9";
+            toggles[i].style.color = "#5429e3";
             icons[i].classList.remove('fa-plus');
             icons[i].classList.add('fa-minus');
         }
         else{
             contentDiv[i].style.height = "0px";
-            toggles[i].style.color = "#111130";
+            toggles[i].style.color = "#a49be8";
             icons[i].classList.remove('fa-minus');
             icons[i].classList.add('fa-plus');
         }
@@ -20,7 +20,7 @@ for(let i=0; i<toggles.length; i++){
         for(let j=0; j<contentDiv.length; j++){
             if(j!==i){
                 contentDiv[j].style.height = "0px";
-                toggles[j].style.color = "#111130";
+                toggles[j].style.color = "#a49be8";
                 icons[j].classList.remove('fa-minus');
                 icons[j].classList.add('fa-plus');
             }
